1acb287a1ce3df4ee3cf851b1ab1e1e1354e9dcb
Anibrata Saha <broto76@gmail.com>
2020-05-16 16:16:32 +0530

    Added a threshold for round-off

--- VotingClassifier1.py
+++ VotingClassifier1.py
@@ -29,14 +29,6 @@ class VotingClassifier:
     classifier_SVM = None
     classifier_RFC = None
     
-    Y_Predict_KNN = 0
-    Y_Predict_SVM = 0
-    Y_Predict_RFC = 0
-    
-    accuracy_KNN = 0
-    accuracy_SVM = 0
-    accuracy_RFC = 0
-    
     Weight_KNN = 0
     Weight_SVM = 0
     Weight_RFC = 0
@@ -67,12 +59,16 @@ class VotingClassifier:
         self.classifier_KNN.fit(self.X_Train, self.Y_Train)
         self.classifier_SVM.fit(self.X_Train, self.Y_Train)
         self.classifier_RFC.fit(self.X_Train, self.Y_Train)
-        
+    
+    
+    # Not Used
     def internalClassifierPredict(self):
         self.Y_Predict_KNN = self.classifier_KNN.predict(self.X_Test)
         self.Y_Predict_SVM = self.classifier_SVM.predict(self.X_Test)
         self.Y_Predict_RFC = self.classifier_RFC.predict(self.X_Test)
+    
         
+    # Not Used
     def calculateClassifierWeights(self):
         self.accuracy_KNN = accuracy_score(self.Y_Test, self.Y_Predict_KNN, normalize=False)
         self.accuracy_SVM = accuracy_score(self.Y_Test, self.Y_Predict_SVM, normalize=False)
@@ -81,6 +77,19 @@ class VotingClassifier:
         self.Weight_KNN = (self.accuracy_KNN) / (self.accuracy_KNN + self.accuracy_SVM + self.accuracy_RFC)
         self.Weight_SVM = (self.accuracy_SVM) / (self.accuracy_KNN + self.accuracy_SVM + self.accuracy_RFC)
         self.Weight_RFC = (self.accuracy_RFC) / (self.accuracy_KNN + self.accuracy_SVM + self.accuracy_RFC)
+        
+    def internalAssignAvgWeight(self):
+        Y_Predict_KNN = self.classifier_KNN.predict(self.X_Test)
+        Y_Predict_SVM = self.classifier_SVM.predict(self.X_Test)
+        Y_Predict_RFC = self.classifier_RFC.predict(self.X_Test)
+        
+        accuracy_KNN = accuracy_score(self.Y_Test, Y_Predict_KNN, normalize=False)
+        accuracy_SVM = accuracy_score(self.Y_Test, Y_Predict_SVM, normalize=False)
+        accuracy_RFC = accuracy_score(self.Y_Test, Y_Predict_RFC, normalize=False)
+        
+        self.Weight_KNN = (accuracy_KNN) / (accuracy_KNN + accuracy_SVM + accuracy_RFC)
+        self.Weight_SVM = (accuracy_SVM) / (accuracy_KNN + accuracy_SVM + accuracy_RFC)
+        self.Weight_RFC = (accuracy_RFC) / (accuracy_KNN + accuracy_SVM + accuracy_RFC)
     
     def fit_func(self, features, result):
         
@@ -90,11 +99,18 @@ class VotingClassifier:
         # Split the data to training and test set
         self.splitData(features, result)
         self.internalClassifierFit()
-        self.internalClassifierPredict()
-        self.calculateClassifierWeights()
+        # self.internalClassifierPredict()
+        # self.calculateClassifierWeights()
+        self.internalAssignAvgWeight()    
         
         
-    def predict_func(self, test_features):
+    def showInternalAccuracy(self,expected):
+        self.accuracy_KNN_Test = accuracy_score(expected, self.KNN_Data, normalize=True)
+        self.accuracy_SVM_Test = accuracy_score(expected, self.SVM_Data, normalize=True)
+        self.accuracy_RFC_Test = accuracy_score(expected, self.RFC_Data, normalize=True)
+        
+        
+    def predict_on_weight(self, test_features, threshold):
         test_features = self.pre_prepossing(test_features)
         self.KNN_Data = self.classifier_KNN.predict(test_features)
         self.SVM_Data = self.classifier_SVM.predict(test_features)
@@ -103,29 +119,38 @@ class VotingClassifier:
         self.result_internal = (self.Weight_KNN*self.KNN_Data) + (self.Weight_SVM*self.SVM_Data) + (self.Weight_RFC * self.RFC_Data)
         
         for i in range (0, self.result_internal.size):
-            if (self.result_internal[i] == 0 or self.result_internal[i] == 1):
+            diff = self.result_internal[i] - int(self.result_internal[i])
+            if (diff == 0):
                 continue
-            if self.result_internal[i] > 0.5:
-                self.result_internal[i] = 1
-                print (i)
+            print (i)
+            if (diff >= threshold):
+                self.result_internal[i] = int(self.result_internal[i]) + 1
             else:
-                self.result_internal[i] = 0
-                print (i)
+                self.result_internal[i] = int(self.result_internal[i])
                 
-        return self.result_internal        
+        return self.result_internal  
+
+        
+    def predict_on_weighted_votes(self, test_features, threshold)
         
 
 
 classifier = VotingClassifier()
-dataset = pd.read_csv("BreastCancer.csv")
+path = "D:/Pyhton ML/VotingClassifier/Data/car_evaluation_processed.csv"
+dataset = pd.read_csv(path)
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 10].values
-Y = Y/2 -1
+Y = dataset.iloc[:, 6].values
+# Y = Y/2 -1
 X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size=0.2)
 classifier.fit_func(X_Train, Y_Train)
-result = classifier.predict_func(X_Test)
-accuracy = accuracy_score(Y_Test, result, normalize=False)
+result = classifier.predict_on_weight(X_Test, 0.5)
+classifier.showInternalAccuracy(Y_Test)
+accuracy_Voting = accuracy_score(Y_Test, result, normalize=True)
 
 
+# classifier_KNN = KNeighborsClassifier()
+# classifier_KNN.fit(X_Train, Y_Train)
+# output = classifier_KNN.predict(X_Test)
+# accuracy = accuracy_score(Y_Test, output, normalize=True)
 
 
